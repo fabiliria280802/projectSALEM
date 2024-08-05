26fa6f7ccf665c633b540098b62a6a6cb0196b3e
Fabiana Liria Soto <117101392+fabiliria280802@users.noreply.github.com>
2024-08-04 19:50:48 -0500

    chore: Update package.json with new dependencies

---
+++ frontend/src/services/authService.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:5000/api/auth/';
+
+const login = (username, password) => {
+  return axios.post(API_URL + 'login', { username, password })
+    .then(response => {
+      if (response.data.token) {
+        localStorage.setItem('user', JSON.stringify(response.data));
+      }
+      return response.data;
+    });
+};
+
+const logout = () => {
+  localStorage.removeItem('user');
+};
+
+export default {
+  login,
+  logout
+};
