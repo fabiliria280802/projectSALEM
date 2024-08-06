9b2d23b182e390e5efd8360f45c8a68bbece26bb
Fabiana Liria Soto <117101392+fabiliria280802@users.noreply.github.com>
2024-08-06 12:48:24 -0500

    chore: Remove unused CSS imports in Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,7 +1,3 @@
-import 'primereact/resources/themes/saga-blue/theme.css';
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
-
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import authService from '../services/authService';
