8a6f3593f34404617a53953851d8a95e1b69d2f0
Fabiana Liria Soto <117101392+fabiliria280802@users.noreply.github.com>
2024-08-06 11:06:50 -0500

    chore: Update jest.config.js and jest.setup.js for frontend tests

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
 	collectCoverage: false,
 	collectCoverageFrom: ['src/**/*.{js,jsx}'],
 	coverageDirectory: 'coverage',
-	testEnvironment: 'jsdom',
+	testEnvironment: 'jest-environment-jsdom',
 	setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-};
+  };
