2493bcad65015704f8d17dfdf9a5db1c3a29b007
Fabiana Liria Soto <117101392+fabiliria280802@users.noreply.github.com>
2024-08-05 22:16:23 -0500

    chore: Update eslint-plugin-react to latest version

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -11,7 +11,7 @@ module.exports = [
       },
     },
     plugins: {
-      react: require('eslint-plugin-react'),
+      react: 'eslint-plugin-react',
     },
     rules: {
       "no-unused-vars": "error",
