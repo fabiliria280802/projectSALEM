db5fccf1ce866798ffc522a27746596f53b6652b
Fabiana Liria Soto <117101392+fabiliria280802@users.noreply.github.com>
2024-10-20 15:40:36 -0500

    refactor: Update CreateUserPage.js and userService.js for operations like CRUD of users

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -2,25 +2,34 @@ import axios from 'axios';
 
 const API_URL = 'http://localhost:5000/api/auth/';
 
+// Manejar el login en el frontend
 const login = async (email, password) => {
     try {
         const response = await axios.post(API_URL + 'login', { email, password });
         if (response.data.token) {
-            // Guarda el token en localStorage
+            // Almacenar token en localStorage
             localStorage.setItem('token', response.data.token);
+            localStorage.setItem('user', JSON.stringify(response.data.user)); // Opcionalmente, guardar el usuario
         }
         return response.data;
     } catch (error) {
-        console.error('Error en login:', error);
         throw new Error('Error al iniciar sesión');
     }
 };
 
+// Manejar el logout
 const logout = () => {
-    localStorage.removeItem('token'); // Elimina el token de localStorage
+    localStorage.removeItem('token');
+    localStorage.removeItem('user'); // Si guardaste los datos del usuario
+};
+
+// Obtener el token
+const getToken = () => {
+    return localStorage.getItem('token');
 };
 
 export default {
     login,
     logout,
+    getToken,
 };
