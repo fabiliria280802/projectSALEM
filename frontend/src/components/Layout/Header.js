11f2aa8b185c245c912e6075a2796fc258701489
Fabiana Liria Soto <117101392+fabiliria280802@users.noreply.github.com>
2024-10-20 14:52:02 -0500

    refactor: Update Header.js to improve page title and styling
    - Reorder imports and remove unused import
    - Use useAuth hook for authentication state and logout function
    - Update logout function to redirect to login page
    - Update logo click event to use history.push for navigation
    - Add conditional rendering of Sidebar component for mobile screens
    - Define hideMenuItemsIn array for routes to hide menu items
    - Define pageTitles object for mapping routes to page titles
    - Get current page title based on location.pathname
    - Update renderEndButton function to handle authenticated and unauthenticated states
    - Update Menubar component props with appropriate values
    - Remove unused code and fix formatting issues

--- frontend/src/components/Layout/Header.js
+++ frontend/src/components/Layout/Header.js
@@ -1,23 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Sidebar from '../Layout/Sidebar';
 import { Button, Menubar } from 'primereact';
-import authService from '../../services/authService';
 import logo from '../../assets/logo.png';
 import styles from '../../styles/Header.module.css';
 import { useHistory, useLocation } from 'react-router-dom';
+import useAuth from '../../hooks/useAuth'; // Importamos useAuth para usar el estado de autenticación global
 
 const Header = () => {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const { isAuthenticated, logout } = useAuth(); // Usamos isAuthenticated para saber si el usuario está logueado, y logout para cerrar sesión
     const [isMobile, setIsMobile] = useState(false);
-	const history = useHistory();
+    const history = useHistory();
     const location = useLocation();
 
     useEffect(() => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            setIsLoggedIn(true);
-        }
-
         const handleResize = () => {
             setIsMobile(window.innerWidth <= 768);
         };
@@ -29,18 +24,19 @@ const Header = () => {
     }, []);
 
     const handleLogout = () => {
-        authService.logout();
-        setIsLoggedIn(false);
-        window.location.href = '/';
+        logout(); // Usamos la función logout del contexto
+        history.push('/login'); // Redirigimos al usuario a la página de login después de cerrar sesión
     };
 
-	const handleMenuItemClick = (path) => {
+    const handleMenuItemClick = (path) => {
         history.push(path);
     };
-    //TODO: Agregar cada lista de dominios de pestaña que novan a visualizar el menuITem
-    const hideMenuItemsIn = ['/upload-documents', '/create-user','/users-management', '/user-account','/dashboard'];
+
+    // Definir las rutas en las que no quieres mostrar los menuItems
+    const hideMenuItemsIn = ['/upload-documents', '/create-user', '/users-management', '/user-account', '/dashboard'];
     const shouldShowMenuItems = !hideMenuItemsIn.includes(location.pathname);
 
+    // Definir un objeto de mapeo de rutas a nombres de pestañas
     const pageTitles = {
         '/dashboard': 'Dashboard',
         '/create-user': 'Crear Usuario',
@@ -50,6 +46,7 @@ const Header = () => {
         '/another-path': 'Otra Página',
     };
 
+    // Obtener el nombre de la pestaña actual basado en la ruta
     const pageTitle = pageTitles[location.pathname] || 'Nombre de la pestaña';
 
     const start = (
@@ -58,54 +55,55 @@ const Header = () => {
                 src={logo}
                 alt="Logo"
                 style={{ height: '40px' }}
-                onClick={() => (window.location.href = '/')} />
-            {isMobile && isLoggedIn && <Sidebar />}{' '}
+                onClick={() => history.push('/')} // Usamos history.push para navegar a la página principal
+            />
+            {isMobile && isAuthenticated && <Sidebar />}
             {/* Mostrar sidebar solo en pantallas pequeñas */}
         </div>
     );
 
-    const menuItems = isLoggedIn && shouldShowMenuItems
+    const menuItems = isAuthenticated && shouldShowMenuItems
         ? [
               { label: 'Inicio', className: styles.menuItem, command: () => handleMenuItemClick('/') },
               { label: 'Estatus', className: styles.menuItem },
               { label: 'Documentación', className: styles.menuItem },
               { label: 'Entrenamiento', className: styles.menuItem },
               { label: 'Permisos', className: styles.menuItem },
-			  { label: 'Gestion de usuarios', className: styles.menuItem, command: () => handleMenuItemClick('/users-management') },
+              { label: 'Gestión de usuarios', className: styles.menuItem, command: () => handleMenuItemClick('/users-management') },
               { label: 'Cuenta', className: styles.menuItem, command: () => handleMenuItemClick('/user-account') },
           ]
         : [];
 
-const renderEndButton = () => {
-    if (isLoggedIn) {
-        if (shouldShowMenuItems) {
-            return (
-                <Button
-                    label="Logout"
-                    icon="pi pi-sign-out"
-                    className="p-button-secondary"
-                    onClick={handleLogout}
-                />
-            );
+    const renderEndButton = () => {
+        if (isAuthenticated) {
+            if (shouldShowMenuItems) {
+                return (
+                    <Button
+                        label="Logout"
+                        icon="pi pi-sign-out"
+                        className="p-button-secondary"
+                        onClick={handleLogout}
+                    />
+                );
+            } else {
+                return (
+                    <div className={styles.pageContainerTitle}>
+                        <span className={styles.pageTitle}>{pageTitle}</span>
+                    </div>
+                );
+            }
         } else {
             return (
-                <div className={styles.pageContainerTitle}>
-                    <span className={styles.pageTitle}>{pageTitle}</span>
-                </div>
+                <Button
+                    label="Iniciar sesión"
+                    className={styles.buttons}
+                    onClick={() => history.push('/login')}
+                />
             );
         }
-    } else {
-        return (
-            <Button
-                label="Iniciar sesión"
-                className={styles.buttons}
-                onClick={() => (window.location.href = '/login')}
-            />
-        );
-    }
-};
+    };
 
-const headerClass = shouldShowMenuItems ? styles.headerShadow : styles.headerWithImage;
+    const headerClass = shouldShowMenuItems ? styles.headerShadow : styles.headerWithImage;
 
     return (
         <Menubar
--- frontend/src/contexts/AuthContext.js
+++ frontend/src/contexts/AuthContext.js
@@ -1,39 +1,46 @@
 import React, { createContext, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import authService from '../services/authService';
-import { jwtDecode } from 'jwt-decode';
+import {jwtDecode} from 'jwt-decode'; // Asegúrate de importar jwt-decode correctamente
 
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
+    const [isAuthenticated, setIsAuthenticated] = useState(false); // Estado para verificar si el usuario está autenticado
 
     useEffect(() => {
         const token = localStorage.getItem('token');
         if (token) {
             try {
                 const decodedUser = jwtDecode(token);
-				console.log('Datos del token:', decodedUser);
+                console.log('Datos del token:', decodedUser);
                 setUser(decodedUser);
+                setIsAuthenticated(true); // Si hay un token válido, el usuario está autenticado
             } catch (error) {
                 console.error('Error decodificando el token:', error);
                 authService.logout();
+                setIsAuthenticated(false); // Si el token es inválido, desautenticamos al usuario
             }
         }
     }, []);
 
     const login = async (email, password) => {
         const userData = await authService.login(email, password);
-        setUser(jwtDecode(userData.token));
+        setUser(jwtDecode(userData.token)); // Guardamos los datos decodificados del usuario
+        setIsAuthenticated(true); // Indicamos que el usuario está autenticado
+        localStorage.setItem('token', userData.token); // Guardamos el token en el localStorage para persistencia
     };
 
     const logout = () => {
         authService.logout();
         setUser(null);
+        setIsAuthenticated(false); // Cuando cerramos sesión, indicamos que no está autenticado
+        localStorage.removeItem('token'); // Limpiamos el token del localStorage
     };
 
     return (
-        <AuthContext.Provider value={{ user, login, logout }}>
+        <AuthContext.Provider value={{ user, isAuthenticated, login, logout, setIsAuthenticated }}>
             {children}
         </AuthContext.Provider>
     );
