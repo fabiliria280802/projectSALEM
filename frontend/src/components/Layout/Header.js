8c56771a5193952b815d7f2efa6f4ccf0b2441cd
Fabiana Liria Soto <117101392+fabiliria280802@users.noreply.github.com>
2024-10-20 14:42:51 -0500

    refactor: Update Header.js to add dynamic page title and improve styling

--- frontend/src/components/Layout/Header.js
+++ frontend/src/components/Layout/Header.js
@@ -4,12 +4,13 @@ import { Button, Menubar } from 'primereact';
 import authService from '../../services/authService';
 import logo from '../../assets/logo.png';
 import styles from '../../styles/Header.module.css';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 
 const Header = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [isMobile, setIsMobile] = useState(false);
 	const history = useHistory();
+    const location = useLocation();
 
     useEffect(() => {
         const token = localStorage.getItem('token');
@@ -32,20 +33,38 @@ const Header = () => {
         setIsLoggedIn(false);
         window.location.href = '/';
     };
+
 	const handleMenuItemClick = (path) => {
         history.push(path);
     };
+    //TODO: Agregar cada lista de dominios de pestaña que novan a visualizar el menuITem
+    const hideMenuItemsIn = ['/upload-documents', '/create-user','/users-management', '/user-account','/dashboard'];
+    const shouldShowMenuItems = !hideMenuItemsIn.includes(location.pathname);
 
+    const pageTitles = {
+        '/dashboard': 'Dashboard',
+        '/create-user': 'Crear Usuario',
+        '/upload-documents': 'Carga de Documentos',
+        '/users-management': 'Gestión de Usuarios',
+        '/user-account': 'Cuenta de Usuario',
+        '/another-path': 'Otra Página',
+    };
+
+    const pageTitle = pageTitles[location.pathname] || 'Nombre de la pestaña';
 
     const start = (
         <div style={{ display: 'flex', alignItems: 'center' }}>
-            <img src={logo} alt="Logo" style={{ height: '40px' }}  />
+            <img
+                src={logo}
+                alt="Logo"
+                style={{ height: '40px' }}
+                onClick={() => (window.location.href = '/')} />
             {isMobile && isLoggedIn && <Sidebar />}{' '}
             {/* Mostrar sidebar solo en pantallas pequeñas */}
         </div>
     );
 
-    const menuItems = isLoggedIn
+    const menuItems = isLoggedIn && shouldShowMenuItems
         ? [
               { label: 'Inicio', className: styles.menuItem, command: () => handleMenuItemClick('/') },
               { label: 'Estatus', className: styles.menuItem },
@@ -57,30 +76,45 @@ const Header = () => {
           ]
         : [];
 
-    const end = isLoggedIn ? (
-        <Button
-            label="Logout"
-            icon="pi pi-sign-out"
-            className="p-button-secondary"
-            onClick={handleLogout}
-        />
-    ) : (
-        <Button
-            label="Iniciar sesión"
-            className={styles.buttons}
-            onClick={() => (window.location.href = '/login')}
-        />
-    );
+const renderEndButton = () => {
+    if (isLoggedIn) {
+        if (shouldShowMenuItems) {
+            return (
+                <Button
+                    label="Logout"
+                    icon="pi pi-sign-out"
+                    className="p-button-secondary"
+                    onClick={handleLogout}
+                />
+            );
+        } else {
+            return (
+                <div className={styles.pageContainerTitle}>
+                    <span className={styles.pageTitle}>{pageTitle}</span>
+                </div>
+            );
+        }
+    } else {
+        return (
+            <Button
+                label="Iniciar sesión"
+                className={styles.buttons}
+                onClick={() => (window.location.href = '/login')}
+            />
+        );
+    }
+};
+
+const headerClass = shouldShowMenuItems ? styles.headerShadow : styles.headerWithImage;
 
     return (
         <Menubar
-            className={styles.header}
+            className={`${styles.header} ${headerClass}`}
             model={!isMobile ? menuItems : []}
             start={start}
-            end={end}
+            end={renderEndButton()}
         />
     );
 };
 
 export default Header;
-
