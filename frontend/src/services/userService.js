a39ec5ad0d1ba16dc73fe04954e4caad4a29b5cd
mateoudla <mateo.avila@udla.edu.ec>
2024-10-22 13:45:57 -0500

    Ready to MAC

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import authService from './authService';
 
-const API_URL = 'http://localhost:5000/api/users/';
+const API_URL = 'http://localhost:8080/api/users/';
 
 const createUser = async (userData) => {
 	const token = authService.getToken();
